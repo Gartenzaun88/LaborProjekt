6e869a27db19c94d95eec3b14f54ede0ed707db9
Gartenzaun88 <4Gernot.Klien@gmail.com>
2017-09-11 10:36:17 +0200

    r

--- Linux/ReachTest/Main.c
+++ Linux/ReachTest/Main.c
@@ -6,22 +6,29 @@
 #include "Rm101Control.h"
 #include "Rm101Kinematics.h"
 
-int main (){
+int main (int argc, char *argv[]){
    // -225; //Minimal mögliche X Koordinate : -225
    // -440; //Minimal mög440liche Y Koordinate : -440
    // -100; //Minimal mögliche Z Koordiante: -100
+   double Wristpinch = argc > 1 ? atof(argv[1]) : 0.0;
    FILE *FileHandle = fopen("ReachTestFull.txt", "w");
+   FILE *FileHandlePartial = fopen("ReachTestPartial.txt", "w");
    
    //Check auf Positive Koordinaten, Reichweite des Roboters ist maximal 440, Kugelradius kann also nicht größer sein
    for(int X = -225; X < 440; X = X + 5){
       for(int Y = -440; Y < 440; Y = Y + 5){
-         for(int Z = 0; X * X + Y * Y + Z * Z < 360000; Z = Z + 5)
+         for(int Z = -100; X * X + Y * Y + Z * Z < 360000; Z = Z + 5)
          {
-            if (ValidateXYZ(X,Y,Z, 0.0))
+            if (ValidateXYZ(X,Y,Z, Wristpinch)){
                fprintf(FileHandle, "%d %d %d\n",X,Y,Z);
+               if (Y % 110 == 0)
+                  fprintf(FileHandlePartial, "%d %d %d\n",X,Y,Z);
+            }
          }
          fprintf(FileHandle, "\n");
       }
    }
+   fclose(FileHandlePartial);
    fclose(FileHandle);
+
 }
