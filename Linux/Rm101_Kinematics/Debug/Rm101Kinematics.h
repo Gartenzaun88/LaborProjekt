721e276e628e97f4f96bec1d2a63bf8068c4ab5a
Gartenzaun88 <4Gernot.Klien@gmail.com>
2017-09-14 13:40:30 +0200

    Compilerflags korrigiert,  Kinematics kommentiert

--- Linux/Rm101_Kinematics/Debug/Rm101Kinematics.h
+++ Linux/Rm101_Kinematics/Debug/Rm101Kinematics.h
@@ -5,8 +5,26 @@
 #include <math.h>
 #include "Rm101Kinematics.h"
 
+/** Kontrolliert für eine gesammte übergeben Roboterposition ob diese von den Gelenken eingenommen werden kann
+ *  @param[in] aRobotPosition Position die kontrolliert werden soll
+ *  @return True, wenn der sich wie angegeben positionieren kann
+ */
 Bool CheckRobotMovementAll(RobotPosition aRobotPosition);
+
+/** Versucht den TCP des Roboters an eine gegene Koordinate zu fahren und setzt den Errorcode
+ *  Neigung des TCP im Verhältnis zur horizontalen, sowie dessen Rotation müssen ebenfalls übergeben werden
+ *  @param[in] X,Y,Z Koordinaten des anzufahrenden Punktes
+ *  @param[in] aWristPitch Neigung des TCP gegenüber der horizontalen, Positiv nach oben geneigt
+ *  @param[in] aWristRoll Rotation des TCP
+ *  @return True, wenn der Roboter die angegebene Position anfährt
+ */
 Bool MoveRoboterXYZ(int16_t X, int16_t Y, int16_t Z, double_t aWristPitch, double_t aWristRoll);
+
+/** Prüft ob der Roboter eine bestimmte Koordinate mit der übergeben Neigung des TCP anfahren kann
+ *  @param[in] X,Y,Z Koordinaten des anzufahrenden Punktes
+ *  @param[in] aWristPitch Neigung des TCP gegenüber der horizontalen, Positiv nach oben geneigt
+ *  @return True, wenn der Roboter die angegebene Position anfahren kann
+ */
 Bool ValidateXYZ(int16_t X, int16_t Y, int16_t Z, double_t aWristPitch);
 
 #endif /* KINEMATIC_H */
